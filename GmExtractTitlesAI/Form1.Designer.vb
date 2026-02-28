304649c68517fc77e8720515de3fba50dec2f4f7
George Matzouranis <gmlogic@gmail.com>
2026-02-28 16:23:10 +0200

    Implement TIFF OCR workflow and title extraction UI

--- GmExtractTitlesAI/Form1.Designer.vb
+++ GmExtractTitlesAI/Form1.Designer.vb
@@ -2,7 +2,6 @@
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
@@ -14,18 +13,201 @@ Partial Class Form1
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnLoadTiff = New System.Windows.Forms.Button()
+        Me.lblFiles = New System.Windows.Forms.Label()
+        Me.lstSelectedFiles = New System.Windows.Forms.ListBox()
+        Me.btnOcrPages = New System.Windows.Forms.Button()
+        Me.btnConvertToMonotonic = New System.Windows.Forms.Button()
+        Me.btnExtractTitles = New System.Windows.Forms.Button()
+        Me.tableMain = New System.Windows.Forms.TableLayoutPanel()
+        Me.txtRawPolytonic = New System.Windows.Forms.TextBox()
+        Me.txtCleanMonotonic = New System.Windows.Forms.TextBox()
+        Me.txtExtractedTitles = New System.Windows.Forms.TextBox()
+        Me.lblRawText = New System.Windows.Forms.Label()
+        Me.lblCleanText = New System.Windows.Forms.Label()
+        Me.lblTitles = New System.Windows.Forms.Label()
+        Me.tableMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnLoadTiff
+        '
+        Me.btnLoadTiff.Location = New System.Drawing.Point(12, 12)
+        Me.btnLoadTiff.Name = "btnLoadTiff"
+        Me.btnLoadTiff.Size = New System.Drawing.Size(155, 30)
+        Me.btnLoadTiff.TabIndex = 0
+        Me.btnLoadTiff.Text = "Load TIFF pages"
+        Me.btnLoadTiff.UseVisualStyleBackColor = True
+        '
+        'lblFiles
+        '
+        Me.lblFiles.AutoSize = True
+        Me.lblFiles.Location = New System.Drawing.Point(176, 20)
+        Me.lblFiles.Name = "lblFiles"
+        Me.lblFiles.Size = New System.Drawing.Size(87, 16)
+        Me.lblFiles.TabIndex = 1
+        Me.lblFiles.Text = "Loaded files:"
+        '
+        'lstSelectedFiles
+        '
+        Me.lstSelectedFiles.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+        Me.lstSelectedFiles.FormattingEnabled = True
+        Me.lstSelectedFiles.ItemHeight = 16
+        Me.lstSelectedFiles.Location = New System.Drawing.Point(12, 48)
+        Me.lstSelectedFiles.Name = "lstSelectedFiles"
+        Me.lstSelectedFiles.Size = New System.Drawing.Size(1239, 84)
+        Me.lstSelectedFiles.TabIndex = 2
+        '
+        'btnOcrPages
+        '
+        Me.btnOcrPages.Location = New System.Drawing.Point(12, 138)
+        Me.btnOcrPages.Name = "btnOcrPages"
+        Me.btnOcrPages.Size = New System.Drawing.Size(155, 30)
+        Me.btnOcrPages.TabIndex = 3
+        Me.btnOcrPages.Text = "OCR all pages"
+        Me.btnOcrPages.UseVisualStyleBackColor = True
+        '
+        'btnConvertToMonotonic
+        '
+        Me.btnConvertToMonotonic.Location = New System.Drawing.Point(173, 138)
+        Me.btnConvertToMonotonic.Name = "btnConvertToMonotonic"
+        Me.btnConvertToMonotonic.Size = New System.Drawing.Size(205, 30)
+        Me.btnConvertToMonotonic.TabIndex = 4
+        Me.btnConvertToMonotonic.Text = "Convert + clean text"
+        Me.btnConvertToMonotonic.UseVisualStyleBackColor = True
+        '
+        'btnExtractTitles
+        '
+        Me.btnExtractTitles.Location = New System.Drawing.Point(384, 138)
+        Me.btnExtractTitles.Name = "btnExtractTitles"
+        Me.btnExtractTitles.Size = New System.Drawing.Size(155, 30)
+        Me.btnExtractTitles.TabIndex = 5
+        Me.btnExtractTitles.Text = "Extract titles"
+        Me.btnExtractTitles.UseVisualStyleBackColor = True
+        '
+        'tableMain
+        '
+        Me.tableMain.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tableMain.ColumnCount = 3
+        Me.tableMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.tableMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.tableMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.tableMain.Controls.Add(Me.lblRawText, 0, 0)
+        Me.tableMain.Controls.Add(Me.lblCleanText, 1, 0)
+        Me.tableMain.Controls.Add(Me.lblTitles, 2, 0)
+        Me.tableMain.Controls.Add(Me.txtRawPolytonic, 0, 1)
+        Me.tableMain.Controls.Add(Me.txtCleanMonotonic, 1, 1)
+        Me.tableMain.Controls.Add(Me.txtExtractedTitles, 2, 1)
+        Me.tableMain.Location = New System.Drawing.Point(12, 174)
+        Me.tableMain.Name = "tableMain"
+        Me.tableMain.RowCount = 2
+        Me.tableMain.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26.0!))
+        Me.tableMain.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tableMain.Size = New System.Drawing.Size(1239, 483)
+        Me.tableMain.TabIndex = 6
+        '
+        'txtRawPolytonic
+        '
+        Me.txtRawPolytonic.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtRawPolytonic.Location = New System.Drawing.Point(3, 29)
+        Me.txtRawPolytonic.Multiline = True
+        Me.txtRawPolytonic.Name = "txtRawPolytonic"
+        Me.txtRawPolytonic.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtRawPolytonic.Size = New System.Drawing.Size(407, 451)
+        Me.txtRawPolytonic.TabIndex = 3
+        '
+        'txtCleanMonotonic
+        '
+        Me.txtCleanMonotonic.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtCleanMonotonic.Location = New System.Drawing.Point(416, 29)
+        Me.txtCleanMonotonic.Multiline = True
+        Me.txtCleanMonotonic.Name = "txtCleanMonotonic"
+        Me.txtCleanMonotonic.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtCleanMonotonic.Size = New System.Drawing.Size(407, 451)
+        Me.txtCleanMonotonic.TabIndex = 4
+        '
+        'txtExtractedTitles
+        '
+        Me.txtExtractedTitles.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtExtractedTitles.Location = New System.Drawing.Point(829, 29)
+        Me.txtExtractedTitles.Multiline = True
+        Me.txtExtractedTitles.Name = "txtExtractedTitles"
+        Me.txtExtractedTitles.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtExtractedTitles.Size = New System.Drawing.Size(407, 451)
+        Me.txtExtractedTitles.TabIndex = 5
+        '
+        'lblRawText
+        '
+        Me.lblRawText.AutoSize = True
+        Me.lblRawText.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblRawText.Location = New System.Drawing.Point(3, 0)
+        Me.lblRawText.Name = "lblRawText"
+        Me.lblRawText.Size = New System.Drawing.Size(407, 26)
+        Me.lblRawText.TabIndex = 0
+        Me.lblRawText.Text = "OCR text (polytonic)"
+        Me.lblRawText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblCleanText
+        '
+        Me.lblCleanText.AutoSize = True
+        Me.lblCleanText.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblCleanText.Location = New System.Drawing.Point(416, 0)
+        Me.lblCleanText.Name = "lblCleanText"
+        Me.lblCleanText.Size = New System.Drawing.Size(407, 26)
+        Me.lblCleanText.TabIndex = 1
+        Me.lblCleanText.Text = "Monotonic + cleaned"
+        Me.lblCleanText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblTitles
+        '
+        Me.lblTitles.AutoSize = True
+        Me.lblTitles.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblTitles.Location = New System.Drawing.Point(829, 0)
+        Me.lblTitles.Name = "lblTitles"
+        Me.lblTitles.Size = New System.Drawing.Size(407, 26)
+        Me.lblTitles.TabIndex = 2
+        Me.lblTitles.Text = "Extracted paragraph titles"
+        Me.lblTitles.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1263, 669)
+        Me.Controls.Add(Me.tableMain)
+        Me.Controls.Add(Me.btnExtractTitles)
+        Me.Controls.Add(Me.btnConvertToMonotonic)
+        Me.Controls.Add(Me.btnOcrPages)
+        Me.Controls.Add(Me.lstSelectedFiles)
+        Me.Controls.Add(Me.lblFiles)
+        Me.Controls.Add(Me.btnLoadTiff)
+        Me.MinimumSize = New System.Drawing.Size(1080, 550)
+        Me.Name = "Form1"
+        Me.Text = "GmExtractTitlesAI"
+        Me.tableMain.ResumeLayout(False)
+        Me.tableMain.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnLoadTiff As Button
+    Friend WithEvents lblFiles As Label
+    Friend WithEvents lstSelectedFiles As ListBox
+    Friend WithEvents btnOcrPages As Button
+    Friend WithEvents btnConvertToMonotonic As Button
+    Friend WithEvents btnExtractTitles As Button
+    Friend WithEvents tableMain As TableLayoutPanel
+    Friend WithEvents txtRawPolytonic As TextBox
+    Friend WithEvents txtCleanMonotonic As TextBox
+    Friend WithEvents txtExtractedTitles As TextBox
+    Friend WithEvents lblRawText As Label
+    Friend WithEvents lblCleanText As Label
+    Friend WithEvents lblTitles As Label
 End Class
--- GmExtractTitlesAI/Form1.vb
+++ GmExtractTitlesAI/Form1.vb
@@ -1,3 +1,236 @@
-﻿Public Class Form1
+﻿Option Strict On
+
+Imports System.Diagnostics
+Imports System.Globalization
+Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private ReadOnly imageFiles As New List(Of String)()
+
+    Private Sub btnLoadTiff_Click(sender As Object, e As EventArgs) Handles btnLoadTiff.Click
+        Dim selected As New List(Of String)()
+
+        Using dialog As New OpenFileDialog()
+            dialog.Title = "Select TIFF files"
+            dialog.Filter = "TIFF files (*.tif;*.tiff)|*.tif;*.tiff|All files (*.*)|*.*"
+            dialog.Multiselect = True
+
+            Dim result = dialog.ShowDialog(Me)
+            If result = DialogResult.OK Then
+                selected.AddRange(dialog.FileNames)
+            End If
+        End Using
+
+        If selected.Count = 0 Then
+            selected.AddRange(GetDefaultImageFiles())
+        End If
+
+        imageFiles.Clear()
+        imageFiles.AddRange(selected.Distinct(StringComparer.OrdinalIgnoreCase))
+        RefreshSelectedFileList()
+    End Sub
+
+    Private Sub btnOcrPages_Click(sender As Object, e As EventArgs) Handles btnOcrPages.Click
+        If imageFiles.Count = 0 Then
+            imageFiles.AddRange(GetDefaultImageFiles())
+            RefreshSelectedFileList()
+        End If
+
+        Dim missing = imageFiles.Where(Function(path) Not File.Exists(path)).ToList()
+        If missing.Count > 0 Then
+            MessageBox.Show("Missing files:" & Environment.NewLine & String.Join(Environment.NewLine, missing), "File error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim pagesText As New StringBuilder()
+
+        For i = 0 To imageFiles.Count - 1
+            Dim filePath = imageFiles(i)
+            Dim ocr = RunTesseractOcr(filePath)
+
+            If String.IsNullOrWhiteSpace(ocr) Then
+                ocr = $"[No OCR output for page {i + 1}]"
+            End If
+
+            pagesText.AppendLine($"===== PAGE {i + 1}: {Path.GetFileName(filePath)} =====")
+            pagesText.AppendLine(ocr.Trim())
+            pagesText.AppendLine()
+        Next
+
+        txtRawPolytonic.Text = pagesText.ToString().Trim()
+    End Sub
+
+    Private Sub btnConvertToMonotonic_Click(sender As Object, e As EventArgs) Handles btnConvertToMonotonic.Click
+        Dim raw = txtRawPolytonic.Text
+        Dim converted = ConvertPolytonicToMonotonic(raw)
+        Dim cleaned = CleanTextForProcessing(converted)
+        txtCleanMonotonic.Text = cleaned
+    End Sub
+
+    Private Sub btnExtractTitles_Click(sender As Object, e As EventArgs) Handles btnExtractTitles.Click
+        Dim source = txtCleanMonotonic.Text
+        If String.IsNullOrWhiteSpace(source) Then
+            MessageBox.Show("Run OCR and convert text first.", "No input", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+        Dim titles = ExtractParagraphTitles(source)
+        txtExtractedTitles.Text = String.Join(Environment.NewLine, titles)
+    End Sub
+
+    Private Function GetDefaultImageFiles() As IEnumerable(Of String)
+        Return New String() {
+            "Q:\Vellas\Vellas-0331.tif",
+            "Q:\Vellas\Vellas-0332.tif",
+            "Q:\Vellas\Vellas-0333.tif",
+            "Q:\Vellas\Vellas-0334.tif",
+            "Q:\Vellas\Vellas-0335.tif"
+        }
+    End Function
+
+    Private Sub RefreshSelectedFileList()
+        lstSelectedFiles.Items.Clear()
+        For Each filePath In imageFiles
+            lstSelectedFiles.Items.Add(filePath)
+        Next
+    End Sub
+
+    Private Function RunTesseractOcr(filePath As String) As String
+        If Not IsTesseractAvailable() Then
+            Return "[tesseract executable not found. Install Tesseract OCR and add it to PATH. Try language: ell.]"
+        End If
+
+        Dim tempOutputBase = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"))
+        Dim outputTxt = tempOutputBase & ".txt"
+
+        Try
+            Dim psi As New ProcessStartInfo()
+            psi.FileName = "tesseract"
+            psi.Arguments = $"""{filePath}"" ""{tempOutputBase}"" -l ell"
+            psi.CreateNoWindow = True
+            psi.UseShellExecute = False
+            psi.RedirectStandardError = True
+            psi.RedirectStandardOutput = True
+
+            Using proc = Process.Start(psi)
+                If proc Is Nothing Then
+                    Return String.Empty
+                End If
+
+                proc.WaitForExit(120000)
+                If Not proc.HasExited Then
+                    proc.Kill()
+                    Return "[OCR timeout]"
+                End If
+
+                If proc.ExitCode <> 0 Then
+                    Dim err = proc.StandardError.ReadToEnd()
+                    Return $"[OCR failed: {err}]"
+                End If
+            End Using
+
+            If File.Exists(outputTxt) Then
+                Return File.ReadAllText(outputTxt)
+            End If
+
+            Return String.Empty
+        Catch ex As Exception
+            Return $"[OCR exception: {ex.Message}]"
+        Finally
+            If File.Exists(outputTxt) Then
+                File.Delete(outputTxt)
+            End If
+        End Try
+    End Function
+
+    Private Function IsTesseractAvailable() As Boolean
+        Try
+            Dim psi As New ProcessStartInfo()
+            psi.FileName = "tesseract"
+            psi.Arguments = "--version"
+            psi.CreateNoWindow = True
+            psi.UseShellExecute = False
+            psi.RedirectStandardOutput = True
+            psi.RedirectStandardError = True
+
+            Using proc = Process.Start(psi)
+                If proc Is Nothing Then
+                    Return False
+                End If
+
+                proc.WaitForExit(5000)
+                Return proc.ExitCode = 0
+            End Using
+        Catch
+            Return False
+        End Try
+    End Function
+
+    Private Function ConvertPolytonicToMonotonic(input As String) As String
+        If String.IsNullOrEmpty(input) Then
+            Return String.Empty
+        End If
+
+        Dim normalized = input.Normalize(NormalizationForm.FormD)
+        Dim sb As New StringBuilder()
+
+        For Each ch In normalized
+            Dim cat = CharUnicodeInfo.GetUnicodeCategory(ch)
+            If cat <> UnicodeCategory.NonSpacingMark Then
+                sb.Append(ch)
+            End If
+        Next
+
+        Dim withoutMarks = sb.ToString().Normalize(NormalizationForm.FormC)
+        withoutMarks = withoutMarks.Replace("ς", "σ")
+        Return withoutMarks
+    End Function
+
+    Private Function CleanTextForProcessing(input As String) As String
+        If String.IsNullOrWhiteSpace(input) Then
+            Return String.Empty
+        End If
+
+        Dim allowedPattern = "[^\p{L}\p{N}\p{P}\p{Z}\r\n]"
+        Dim cleaned = Regex.Replace(input, allowedPattern, " ")
+
+        cleaned = Regex.Replace(cleaned, "[ \t]+", " ")
+        cleaned = Regex.Replace(cleaned, "\n{3,}", Environment.NewLine & Environment.NewLine)
+
+        Return cleaned.Trim()
+    End Function
+
+    Private Function ExtractParagraphTitles(cleanText As String) As List(Of String)
+        Dim lines = cleanText.Split({Environment.NewLine}, StringSplitOptions.None)
+        Dim titles As New List(Of String)()
+
+        For i = 0 To lines.Length - 1
+            Dim line = lines(i).Trim()
+            If line.Length = 0 Then
+                Continue For
+            End If
+
+            Dim prevEmpty = (i = 0) OrElse String.IsNullOrWhiteSpace(lines(i - 1))
+            Dim nextEmpty = (i = lines.Length - 1) OrElse String.IsNullOrWhiteSpace(lines(i + 1))
+
+            If Not (prevEmpty AndAlso nextEmpty) Then
+                Continue For
+            End If
+
+            Dim stripped = Regex.Replace(line, "^[^\p{Lu}]*", String.Empty)
+            If stripped.Length = 0 Then
+                Continue For
+            End If
+
+            Dim first = stripped(0)
+            If Char.IsUpper(first) Then
+                titles.Add(stripped)
+            End If
+        Next
+
+        Return titles.Distinct().ToList()
+    End Function
 
 End Class
